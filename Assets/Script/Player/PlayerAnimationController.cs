38b8df10ae2c8b00deb0696f5418013a18578148
Avin19 <avinashy1993@gmail.com>
2025-01-22 21:29:15 +0530

    Working Animation

--- Assets/Script/Player/PlayerAnimationController.cs
+++ Assets/Script/Player/PlayerAnimationController.cs
@@ -15,14 +15,20 @@ public class PlayerAnimationController : MonoBehaviour
     private void Start()
     {
         PlayerController.Instance.PlayerAnimator += AnimationPerformed;
+        PlayerController.Instance.IdleAnimation += IdleAnimation;
     }
     private void OnDisable()
     {
         PlayerController.Instance.PlayerAnimator -= AnimationPerformed;
+        PlayerController.Instance.IdleAnimation -= IdleAnimation;
     }
     private void AnimationPerformed(object sender, string e)
     {
         animator.SetBool(e, true);
+    }
+    private void IdleAnimation(object sender, string e)
+    {
+        animator.SetBool(e, false);
     }
 }
 
