eef9cf3cf0514f08a53c061752831d6612c81474
Avin19 <avinashy1993@gmail.com>
2025-02-04 00:11:22 +0530

    Working on Shop manager logic

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -72,10 +72,6 @@ public class GameManager : MonoBehaviour
         gameState = state;
         onGameStateChanged?.Invoke(gameState);
 
-        switch (gameState)
-        {
-
-        }
 
 
     }
