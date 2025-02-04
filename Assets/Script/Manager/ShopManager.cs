8e9f449c159548df1f6a93b8a615c4435f4de409
Avin19 <avinashy1993@gmail.com>
2025-02-04 23:11:27 +0530

    Working on Button unclock

--- Assets/Script/Manager/ShopManager.cs
+++ Assets/Script/Manager/ShopManager.cs
@@ -14,16 +14,23 @@ public class ShopManager : MonoBehaviour
     private void Start()
     {
         ConfigureButton();
+        UnlockSkin(5);
     }
 
     private void ConfigureButton()
     {
         for (int i = 0; i < skinButtons.Length; i++)
         {
-            skinButtons[i].Configure(skinArray[i], true);
+            bool unlocked = PlayerPrefs.GetInt("skinButton" + i) == 1;
+            skinButtons[i].Configure(skinArray[i], unlocked);
             int skinIndex = i;
             skinButtons[i].GetButton().onClick.AddListener(() => SelectSkin(skinIndex));
         }
+    }
+    public void UnlockSkin(int skinIndex)
+    {
+        PlayerPrefs.SetInt("skinButton" + skinIndex, 1);
+        skinButtons[skinIndex].Unlocked();
     }
     private void SelectSkin(int skinIndex)
     {
